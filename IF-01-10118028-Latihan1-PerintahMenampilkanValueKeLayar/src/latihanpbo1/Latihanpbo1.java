f3ae0a0d62f5469e20fc1d75c68a9e1d973266fd
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:01:47 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan1-PerintahMenampilkanValueKeLayar/src/latihanpbo1/Latihanpbo1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanpbo1;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-1 2018
+   NIM       : 10118028
+   DESKRIPSI : Perintah menampilkan value ke layar
+
+ */
+public class Latihanpbo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIOADATA ROCKSTAR" +"\n" + "--------------------");
+        System.out.println("NAMA\t\t: Surya Fakhriry Hasbi");
+        System.out.println("ALAMAT\t\t: Jl.Cihampelas No.56A/35B");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 19 Februari 2000");
+        System.out.println("--------------------");
+        System.out.println("HOBI\t\t: Makan");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Es Teh");
+    }
+    
+}
